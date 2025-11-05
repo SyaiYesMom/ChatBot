560f2c11d5ab5674123742281ad3e41ff3edd49d
Syailendra <maslendra2004@gmail.com>
2025-11-05 21:46:47 +0700

    Update chatbot.py

--- chatbot.py
+++ chatbot.py
@@ -1,36 +1,13 @@
 import logging
-import google.generativeai as genai
-from telegram import Update
-from telegram.ext import Application, CommandHandler, MessageHandler, filters, ContextTypes
+from telegram.ext import Application, CommandHandler, MessageHandler, filters
 from config import config
-from parse import parshing
+from tele.start import start
+from tele.chat import chat
+from tele.errors import error_handler
 
 logging.basicConfig(
     format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO
 )
-genai.configure(api_key=config.GEMINI_API_KEY)
-model = genai.GenerativeModel("gemini-2.5-flash")
-
-async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text(
-        "Halo! Aku chatbot berbasis AI\nYang dibikin kelompok 6 >///<\n"
-        "Kalau kamu butuh bantuan, bilang yaa :3\n"
-    )
-
-async def chat(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    user_message = update.message.text
-    try:
-        response = model.generate_content(user_message)
-        parsed_response = parshing.remove_bold_and_header(response.text)
-        await update.message.reply_text(parsed_response)
-    except Exception as e:
-        await update.message.reply_text(f"❌ Maaf, terjadi error: {str(e)}")
-        logging.error(f"Error in chat function: {str(e)}")
-
-async def error_handler(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    logging.error(f"Exception while handling an update: {context.error}")
-    if update and update.effective_message:
-        await update.effective_message.reply_text("❌ Terjadi error yang tidak terduga. Silakan coba lagi.")
 
 def main():
     app = Application.builder().token(config.TELEGRAM_TOKEN).build()
@@ -39,7 +16,7 @@ def main():
     app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, chat))
     app.add_error_handler(error_handler)
 
-    print("🤖 Bot running...")
+    print("Bot running...")
     app.run_polling()
 
 if __name__ == "__main__":
